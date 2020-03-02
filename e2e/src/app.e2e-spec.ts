abc0a7a679dbd4d2707423cdf67855ffa07f9300
hoacodai000 <trungvq94@gmail.com>
2020-03-02 22:08:47 +0700

    change title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular9-global-handle-error app is running!');
+    expect(page.getTitleText()).toEqual('angular-ngrx-example app is running!');
   });
 
   afterEach(async () => {
